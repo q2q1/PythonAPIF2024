629cabbf348e714c8900c2a5307070396f53be8a
Junxiao <cjx520kdd@gmail.com>
2024-10-01 06:00:36 +0800

    test

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify 
+from pymongo import MongoClient
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def home():
+	return "HOME"
+
+client = MongoClient("mongodb+srv://cjx520kdd:VpyZ0MQMWCx6VlHR@cluster0.gshvv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client['LS3FALL2024']
+users_collection = db['Users']
+
+print(users_collection)
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = []
+    for user in users_collection.find():
+        user['_id'] = str(user['_id'])  
+        users.append(user)
+    return jsonify(users)
+
+if __name__ == '__main__':
+    app.run(debug=True)
